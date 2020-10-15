f9cea6a84fb58cea78c200c2e410b87e4446f920
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-14 23:10:51 -0500

    Reduce and ViewCount classes

--- src/Reduce.java
+++ src/Reduce.java
@@ -13,9 +13,8 @@ public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException {
         int count = 0;
-        for (IntWritable value : values) {
+        for (IntWritable value : values)
             count += value.get();
-        }
         context.write(key, new IntWritable(count));
     }
 }
